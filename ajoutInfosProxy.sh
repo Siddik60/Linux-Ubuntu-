1059cf583d94b70ca2fe6fca29b461899985ec16
Siddik60 <50698914+Siddik60@users.noreply.github.com>
2019-05-16 11:56:47 +0200

    Update and rename README.md to ajoutInfosProxy.sh

---
+++ ajoutInfosProxy.sh
@@ -0,0 +1,4 @@
+# Linux-Ubuntu
+
+sudo touch /etc/apt/aptconf.d/70debconf >> "Acquire:http::proxy "http://0.0.0.0:0000/";"
+sudo touch /etc/apt/aptconf.d/proxy >> "Acquire::http::proxy "http://0.0.0.0:0000";"
